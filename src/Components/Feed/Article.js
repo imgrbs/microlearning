e92fb735eb29109f719f3f4064182c4f4e4c1320
Kunanan <kunanan.tw@gmail.com>
2018-07-26 15:08:50 +0700

    change get start button

--- src/Components/Feed/Article.js
+++ src/Components/Feed/Article.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react"
 import styled from "styled-components"
-import { Header, Container, Image as DefaultImage, Segment as DefaultSegment } from "semantic-ui-react"
+import {
+  Header,
+  Container,
+  Image as DefaultImage,
+  Segment as DefaultSegment
+} from "semantic-ui-react"
 import firebase from "../../Tools/firebase"
 import NotFound from "../PageNotFound"
 
@@ -46,13 +51,12 @@ export default class Article extends Component {
     const { content, img, title, isNotFound } = this.state
     return (
       <Container>
-        {isNotFound && <NotFound />}
         <Image fluid centered src={img} />
         <Segment>
+          {isNotFound && <NotFound />}
+
           <Header as='h1'>{title}</Header>
-          <p>
-            {content}
-          </p>
+          <p>{content}</p>
         </Segment>
       </Container>
     )
--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,21 +1,107 @@
-import React from "react"
-import { Image, Grid } from "semantic-ui-react"
+import React, { Component, Fragment } from "react"
+import _ from "lodash"
+import {
+  Image,
+  Grid,
+  Container as defaultContainer,
+  Modal,
+  Header,
+  Button as defaultButton
+} from "semantic-ui-react"
+import styled from "styled-components"
 
-const Logo = () => (
-  <Grid centered>
-    <div style={logoStyle}>
-      <Image src='https://assets.skooldio.com/logo/skooldio_logo_current.png' style={imgStyle} />
-    </div>
-  </Grid>
+import { WithUserConsumer } from "../../Context/UserContext"
+import firebase from "../../Tools/firebase"
+import SignUp from "../Signup"
+import Login from "../Login"
+
+const Container = styled(defaultContainer)`
+  margin-top: 30px;
+`
+const Button = styled(defaultButton)`
+  color: red;
+`
+
+export default WithUserConsumer(
+  class extends Component {
+    state = {
+      open: false,
+      content: false
+    }
+
+    signOut () {
+      firebase.auth().signOut()
+    }
+
+    handleModal = () => {
+      this.setState((prevState, props) => ({
+        open: !prevState.open
+      }))
+    }
+    handleEach = () => {
+      this.setState((prevState, props) => ({
+        content: !prevState.content
+      }))
+    }
+
+    render () {
+      const isLogin = _.isEmpty(this.props.user)
+      const { open, content } = this.state
+      let modelContent
+      if (content) {
+        modelContent = (
+          <Modal.Content>
+            <Modal.Description>
+              <Header as='h1'> Join us</Header>
+              <p>
+                Already have an account?{" "}
+                <span className='link' onClick={this.handleEach}>
+                  Sign in
+                </span>
+              </p>
+              <SignUp closeModal={this.handleModal} />
+            </Modal.Description>
+          </Modal.Content>
+        )
+      } else {
+        modelContent = (
+          <Modal.Content>
+            <Modal.Description>
+              <Header as='h1'> Welcome back</Header>
+              <p>
+                No account?{" "}
+                <span className='link' onClick={this.handleEach}>
+                  Create one
+                </span>
+              </p>
+              <Login closeModal={this.handleModal} />
+            </Modal.Description>
+          </Modal.Content>
+        )
+      }
+      return (
+        <Container>
+          <Grid stretched>
+            <Grid.Column computer={13} mobile={8}>
+              <Image
+                src='https://assets.skooldio.com/logo/skooldio_logo_current.png'
+                size='small'
+              />
+            </Grid.Column>
+
+            <Grid.Column computer={3} mobile={8}>
+              {isLogin ? (
+                <Button onClick={this.handleModal}>Get start</Button>
+              ) : (
+                <Button onClick={this.signOut}>{this.props.user.displayName}</Button>
+              )}{" "}
+            </Grid.Column>
+          </Grid>
+          <Modal open={open} onClose={this.handleModal} closeIcon>
+            {modelContent}
+          </Modal>
+        </Container>
+      )
+    }
+  }
 )
-
-const logoStyle = {
-  marginTop: "30px",
-  marginBottom: "15px"
-}
-
-const imgStyle = {
-  height: "9vh"
-}
-
-export default Logo
